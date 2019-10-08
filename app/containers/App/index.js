f90b472d99aa0a8b3c4823f3fe75d9b21beabb32
canis <pavlouk2012@gmail.com>
2019-10-08 22:54:58 +0300

    Remove default example

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,42 +1,28 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import HistoryPage from 'containers/HistoryPage/Loadable';
-import KafedraPage from 'containers/KafedraPage/Loadable';
-import StudyPage from 'containers/StudyPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import './index.css';
 import GlobalStyle from '../../global-styles';
-
-const AppWrapper = styled.div`
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  flex-direction: column;
-`;
 
 export default function App() {
   return (
-    <AppWrapper className="AppWrapper">
-      <Helmet titleTemplate="%s-Kafedra" defaultTitle="React.js Boilerplate" />
-      <Header />
+    <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/study" component={StudyPage} />
-        <Route path="/kafedra" component={KafedraPage} />
-        <Route path="/history" component={HistoryPage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
+        <Route component={NotFoundPage} />
       </Switch>
-      <Footer />
       <GlobalStyle />
-    </AppWrapper>
+    </div>
   );
 }
--- app/containers/StudyPage/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './index.css';
-import HelloComponent from './Text';
-import Headlines from '../../components/Header_study/main';
-
-export default function StudyPage() {
-  return <HelloComponent />;
-  <Headlines />;
-}
