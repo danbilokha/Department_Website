5248bab1347962e1372fa06ed883d4960ce59207
remarka-net <pavlouk2012@gmail.com>
2019-10-09 16:38:22 +0300

    all-in

--- app/containers/StudyPage/index.js
+++ app/containers/StudyPage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
 import Headlines from '../../components/Study_Header/main';
+import Process from '../../components/Study_process/index';
 
 export default function StudyPage() {
   return (
     <div>
       <Headlines />
+      <Process />
     </div>
   );
 }
