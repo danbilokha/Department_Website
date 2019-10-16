ce39fe0345d2b69943f1b01d383669c8cccf0ebb
remarka-net <pavlouk2012@gmail.com>
2019-10-16 17:09:06 +0300

    New html

--- app/components/Study_SubjectBachelor/index.js
+++ app/components/Study_SubjectBachelor/index.js
@@ -10,83 +10,83 @@ export default class SubjectBachelors extends React.Component {
   render() {
     return (
       <div>
-              {/* <dl>
+        {/* <dl>
   <dt>Fruits I like:</dt>
   <dd>Apples</dd>
   <dd>Bananas</dd>
   <dd>Oranges</dd>
 </dl> */}
-            <dl id="subject_list">
-              <dt>В</dt>
-        <dd><a href="">Вища математика</a></dd>
-        <dd><a href="">Вступ до спеціальності</a></dd>
+        <dl id="subject_list">
+          <dt>В</dt>
+          <dd>
+            <a href="">Вища математика</a>
+          </dd>
+          <dd>
+            <a href="">Вступ до спеціальності</a>
+          </dd>
 
-        <dt>Д</dt>
-        <dd></dd>
-        <dd></dd>
+          <dt>Д</dt>
+          <dd />
+          <dd />
 
-        <dt>Е</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>Е</dt>
+          <dd />
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>З</dt>
-        <dd></dd>
-        <dd></dd>
+          <dt>З</dt>
+          <dd />
+          <dd />
 
-        <dt>I</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>I</dt>
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>M</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>M</dt>
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>Н</dt>
-        <dd></dd>
+          <dt>Н</dt>
+          <dd />
 
-        <dt>O</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>O</dt>
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>П</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>П</dt>
+          <dd />
+          <dd />
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>C</dt>
-        <dd></dd>
-        <dd></dd>
+          <dt>C</dt>
+          <dd />
+          <dd />
 
-        <dt>Т</dt>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
+          <dt>Т</dt>
+          <dd />
+          <dd />
+          <dd />
 
-        <dt>У</dt>
-        <dd></dd>
+          <dt>У</dt>
+          <dd />
 
-        <dt>Ф</dt>
-        <dd></dd>
-
-
-
-
-            </dl>
+          <dt>Ф</dt>
+          <dd />
+        </dl>
       </div>
     );
   }
