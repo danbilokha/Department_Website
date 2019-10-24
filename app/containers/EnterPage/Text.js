ba9ea59bc35086e747f19b80a4b21fb9fe5506c6
VolodymyrSmahliuk <amxleclerk@gmail.com>
2019-10-24 11:29:51 +0300

    To create EnterPage

---
+++ app/containers/EnterPage/Text.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Text.css';
+
+import Img from '../../components/Img/index';
+
+
+import { FormattedMessage } from 'react-intl';
+
+
+export default function Text(){
+    return(
+        <div className = 'text'>
+            
+        </div>
+
+    );
+}
