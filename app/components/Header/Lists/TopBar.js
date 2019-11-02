72fd8d746305c9caf60c3a97a21e0e06b5944f26
Pasha Karlin <pashakarlin1234@gmail.com>
2019-11-02 11:22:32 +0200

    changes in Header

--- app/components/Header/Lists/TopBar.js
+++ app/components/Header/Lists/TopBar.js
@@ -1,8 +1,8 @@
 export const topBar = [
-  { id: 1, title: 'univercity', link: '/' },
-  { id: 2, title: 'workForStudents', link: '/' },
-  { id: 3, title: 'magazine', link: '/' },
-  { id: 4, title: 'scientificWork', link: '/' },
-  { id: 5, title: 'conferences', link: '/' },
-  { id: 6, title: 'graduates', link: '/' },
+  { id: 1, title: 'univercity', link: '/history' },
+  { id: 2, title: 'workForStudents', link: '/kafedra_ITM' },
+  { id: 3, title: 'magazine', link: '/history' },
+  { id: 4, title: 'scientificWork', link: '/donators' },
+  { id: 5, title: 'conferences', link: '/study' },
+  { id: 6, title: 'graduates', link: '/teachers' },
 ];
