973002d1d0216cfdb378e37c5ced8cfb957f2891
Alex01aik <Alex01aik@users.noreply.github.com>
2019-11-21 16:22:47 +0200

    "all teachers"

--- app/containers/TeachersPage/Teacheritem.js
+++ app/containers/TeachersPage/Teacheritem.js
@@ -1,26 +1,24 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-
-export default function Itemwriter({ title, content, id }) {
-  if(content != ""){
-    if(title == "contacts"){
-      if(id == 1){
-        return(
-          <p><span className="ItemTitle">Електронна пошта: </span><a href={content}>{content}</a></p>
-        )
-      }
-      return(
-        <p><span className="ItemTitle">Інформація на порталі Intellect: </span><a href={content}>{content}</a></p>
-
-      )
-      
+export default function Itemwriter({ title, content }) {
+  if (content !== '') {
+    if (title === 'Інформація на порталі Intellect: ') {
+      return (
+        <p>
+          <span className="ItemTitle">{title}</span>
+          <span>
+            <a href="/">{content}</a>
+          </span>
+        </p>
+      );
     }
-    return(
-      <p><span className="ItemTitle">{title}</span><span>{content}</span></p>
-    )
-    
+    return (
+      <p>
+        <span className="ItemTitle">{title}</span>
+        <span>{content}</span>
+      </p>
+    );
   }
   return null;
 }
-
-
