33c986b4ed3b657047c29ece4f34ba531bea35d0
VolodymyrSmahliuk <amxleclerk@gmail.com>
2019-11-25 14:23:00 +0200

    Create EducationalProgramPage

--- app/components/TermDescription/index.js
+++ app/components/TermDescription/index.js
@@ -5,7 +5,7 @@ function TermDescription(props) {
   return (
     <p className="degree_content clearfix">
       <b>
-        <i>{props.bold_and_italic_name} </i>
+        {props.bold_and_italic_name}
       </b>
       {props.text}
     </p>
---
+++ app/containers/EducationalProgramPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import Text from './Text';
+
+export default function EducationalProgram() {
+    return(
+        <div className = 'paragraph'>
+            <Text/>
+        </div>
+    )
+}
